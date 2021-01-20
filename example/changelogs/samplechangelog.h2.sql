68e81c93a58ce2464f01d66a62cf20b17371da68
AYUSH <prengineerator@gmail.com>
2021-01-20 15:16:33 +0530

    v2

--- example/changelogs/samplechangelog.h2.sql
+++ example/changelogs/samplechangelog.h2.sql
@@ -38,3 +38,10 @@ CREATE TABLE yourname (
     id int primary key,
     name varchar(50) not null,
 )
+
+--changeset your.name:New
+--rollback DROP TABLE yourname;
+CREATE TABLE yourname1 (
+    id int primary key,
+    name varchar(50) not null,
+)
